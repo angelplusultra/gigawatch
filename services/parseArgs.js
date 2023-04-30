f905fb2085d0d4705e423ac58366627a976fb364
Mac Fitton <macfittondev@gmail.com>
2023-04-30 19:37:32 +0000

    Removed unused constants

--- services/parseArgs.js
+++ services/parseArgs.js
@@ -1,46 +1,35 @@
 const args = process.argv.slice(2); // slice off the first two arguments, which are the node binary and the script file name
-const name = args[1] || "World"; // default to 'World' if no name is provided
-const compiler = args[0];
 export function parseArgs() {
-  let compiler, filePath, cleanMode, argsProvided
+  let compiler, filePath, cleanMode, argsProvided;
 
-  cleanMode = false
-  if(args.length === 0){
-    argsProvided = false
-  } else{
-    argsProvided = true
-  }
-  let flags = {
-    '-c': 'Clean Mode',
-    '-t': 'TypeScript',
-    '-p': 'Python',
-    '-j': 'JavaScript',
-    '-l': 'Lua'
+  cleanMode = false;
+  if (args.length === 0) {
+    argsProvided = false;
+  } else {
+    argsProvided = true;
   }
 
   args.forEach((arg, i) => {
-    if(arg === '-c'){
-      cleanMode = true
+    if (arg === "-c") {
+      cleanMode = true;
     }
-    if(arg === '-j'){
-      compiler = 'JavaScript'
+    if (arg === "-j") {
+      compiler = "JavaScript";
     }
 
-    if(arg === '-t'){
-      compiler = 'TypeScript'
+    if (arg === "-t") {
+      compiler = "TypeScript";
     }
-    if(arg === '-p'){
-      compiler = 'Python'
+    if (arg === "-p") {
+      compiler = "Python";
     }
-    if(arg === '-l'){
-      compiler = 'Lua'
+    if (arg === "-l") {
+      compiler = "Lua";
     }
-    if(arg == "-f"){
-      filePath = args[i + 1]
+    if (arg == "-f") {
+      filePath = args[i + 1];
     }
-  
-    
   });
 
-  return {argsProvided, cleanMode, compiler, filePath }
+  return { argsProvided, cleanMode, compiler, filePath };
 }
--- tests/index.js
+++ tests/index.js
@@ -1,1 +1,1 @@
-console.log("JavaScript")
+console.log("JavaScript");
